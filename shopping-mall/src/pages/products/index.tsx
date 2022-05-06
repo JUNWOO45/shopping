77765790efcf49d841437d409a5ec5edba989f14
junwoo45 <sinyoorang@gmail.com>
2022-05-06 15:05:25 +0900

    feat: react query로 fake api 목업 호출

--- shopping-mall/src/pages/products/index.tsx
+++ shopping-mall/src/pages/products/index.tsx
@@ -1,3 +1,15 @@
-const ProductList = () => <div>상품목록</div>
+import { useQuery } from "react-query"
+import { fetcher, QUERY_KEYS } from "../../queryClients"
+
+const ProductList = () => {
+  const {data} = useQuery(QUERY_KEYS.PRODUCTS, () => {
+    return fetcher({method: 'GET', path: '/products'})
+  })
+
+  console.log('data : ', data)
+
+
+  return (<div>상품목록</div>)
+}
 
 export default ProductList
--- shopping-mall/src/queryClients.ts
+++ shopping-mall/src/queryClients.ts
@@ -7,6 +7,12 @@ import {
   Query,
 } from 'react-query'
 
+const BASE_URL = 'https://fakestoreapi.com'
+
+type AnyObj = {
+  [key:string] : any
+}
+
 // Create a client
 export const getClient = (() => {
   let client: QueryClient | null = null;
@@ -17,3 +23,37 @@ export const getClient = (() => {
     return client
   }
 })();
+
+export const fetcher = async({
+  method,
+  path,
+  body,
+  params
+}: {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
+  path: string;
+  body?: AnyObj;
+  params?: AnyObj
+}) => {
+  try {
+    const url = `${BASE_URL}${path}`;
+    
+    const fetchOptions: RequestInit = {
+      method,
+      headers: {
+        'Content-type': 'application/json',
+        'Access-Control-Allow-Origin': BASE_URL
+      },
+    }
+    const res = await fetch(url, fetchOptions)
+    const json = await res.json();
+
+    return json
+  } catch(e) {
+    console.error(e);
+  }
+}
+
+export const QUERY_KEYS = {
+  PRODUCTS: 'PRODUCTS'
+}
